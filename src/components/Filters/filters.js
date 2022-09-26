7fd68f1a5db031c369435828eea39046084d7087
Katerina Tuckova <tuckova.mail@gmail.com>
2022-09-26 11:28:07 +0200

    new filtering

--- src/components/Filters/filters.js
+++ src/components/Filters/filters.js
@@ -3,71 +3,66 @@ const filters = {
     {
       id: '1',
       name: 'cafe',
-      text: 'Kavárna s koutkem',
+      text: 'Kavárna',
     },
     {
       id: '2',
       name: 'restaurant',
-      text: 'Restaurace s koutkem',
+      text: 'Restaurace',
     },
     {
       id: '3',
-      name: 'outdoors',
-      text: 'Místo s venkovním hřištěm',
-    },
-    {
-      id: '4',
       name: 'playroom',
       text: 'Herna',
     },
     {
-      id: '5',
+      id: '4',
       name: 'babysitting',
       text: 'Místo s hlídáním',
     },
     {
-      id: '6',
+      id: '5',
       name: 'other',
       text: 'Jiné vhodné místo',
     },
   ],
   weather: [
     {
-      id: '7',
+      id: '6',
       name: 'inside',
       text: 'Chci být vevnitř',
     },
     {
-      id: '8',
+      id: '7',
       name: 'outside',
       text: 'Chci být venku',
     },
   ],
   equipment: [
     {
-      id: '9',
+      id: '8',
       name: 'changing-station',
       text: 'Přebalovací pult',
     },
     {
-      id: '10',
+      id: '9',
       name: 'mat',
       text: 'Čistý koberec/podložka',
     },
     {
-      id: '11',
+      id: '10',
       name: 'barrier-free',
       text: 'Bez bariér',
     },
   ],
   price: [
     {
-      id: '12',
+      id: '11',
       name: 'free',
       text: 'Zdarma',
     },
     {
-      id: '13',
+      id: '12',
       name: 'paid',
       text: 'Za vstupné',
     },
