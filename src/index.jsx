9835a7255f29f65293aa6d230a1681f07adb01c9
Katerina Tuckova <tuckova.mail@gmail.com>
2022-10-11 12:15:29 +0200

    GA implemented

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = 'UA-245226528-1';
+ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
